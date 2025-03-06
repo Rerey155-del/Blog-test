2914d4c4b7640cef84c667a4e5c5e4e47026e5fc
Reyhan Maulana <144551736+Rerey155-del@users.noreply.github.com>
2025-03-06 10:33:42 +0700

    oke

--- routes/web.php
+++ routes/web.php
@@ -1,102 +1,15 @@
 <?php
 
-use App\Models\Blog;
-use App\Models\Post;
-
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\BlogController;
 
-
+// Route untuk homepage
 Route::get('/', function () {
-    return view('welcome', [
-        'nama' => "Reyhan Maulana",
-        'title' => "Ini halaman Home yaa",
-    ]);
+    return view('layout.homepage');
 });
 
-
-
-// ======= INI ADALAH CONTOH ROUTE DENGAN MENGGUNAKAN ARRAY ( Data Dummy ) =======
-// Route::get('/blog', function () {
-//     $posts = [
-//         [
-//             'id' => 1,
-//             'title' => 'Judul Artikel 1',
-//             'author' => 'Reyhan Maulana',
-//             'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.'
-//         ],
-//         [
-//             'id' => 2,
-//             'title' => 'Judul Artikel 2',
-//             'author' => 'Nadira Aulia Haq',
-//             'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.'
-//         ]
-//     ];
-
-//     return view('blog', [
-//         'title' => 'ini adalah bagian Blog',
-//         'posts' => $posts
-//     ]);
-// });
-
-// ========= Route untuk menampilkan semua post dari database ========
-Route::get('/blog', function () {
-    $blogs = Blog::all();  // Mengambil semua data blog
-    return view('blog', compact('blogs'));
+// Route untuk halaman login (misalnya ada di views/auth/login.blade.php)
+Route::get('/login', function () {
+    return view('auth.login');
 });
-// ========= Route untuk menampilkan about dari ========
-Route::get('/about', function () {
-    return view('about', [
-        'title' => "Ini halaman about yaa",
-    ]);
-});
-
-
-
-// ======= INI ADALAH CONTOH ROUTE DENGAN MENGGUNAKAN ARRAY (data dummy) =======
-// Route::get('/post/{id}', function ($id) {
-//     $posts = [
-//         [
-//             'id' => 1,
-//             'title' => 'Judul Artikel 1',
-//             'author' => 'Reyhan Maulana',
-//             'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.'
-//         ],
-//         [
-//             'id' => 2,
-//             'title' => 'Judul Artikel 2',
-//             'author' => 'Nadira Aulia Haq',
-//             'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.'
-//         ]
-//     ];
-
-//     // Cari post berdasarkan ID
-//     $post = collect($posts)->firstWhere('id', $id);
-
-//     // Jika tidak ditemukan, tampilkan halaman 404
-//     if (!$post) {
-//         abort(404, 'Post tidak ditemukan');
-//     }
-
-//     return view('post', [
-//         'title' => $post['title'],
-//         'post' => $post
-//     ]);
-// });
-
-// Route untuk menampilkan single blog dari database 
-Route::get('/blog/{id}', function ($id) {
-    $blog = Blog::find($id);
-
-    if (!$blog) {
-        abort(404, 'Blog tidak ditemukan');
-    }
-
-    return view('post', compact('blog'));
-});
-
 
 
