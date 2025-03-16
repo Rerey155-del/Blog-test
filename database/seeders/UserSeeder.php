7a7e047ff5830e8fa5018770c2d7a101d2fca282
Reyhan Maulana <144551736+Rerey155-del@users.noreply.github.com>
2025-03-17 02:45:47 +0700

    oke

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,9 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Illuminate\Foundation\Auth\User;
+use App\Models\User; // Import model User dari App\Models
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class UserSeeder extends Seeder
 {
@@ -14,10 +13,27 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::create([
-            'name' => 'Admin',
-            'email' => 'admin@example.com',
-            'password' => Hash::make('password123')
-        ]);
+        User::firstOrCreate(
+            ['email' => 'admin@example.com'], // Cek apakah email sudah ada
+            [
+                'name' => 'Admin',
+                'password' => Hash::make('password123'),
+            ]
+        );
+
+        User::firstOrCreate(
+            ['email' => 'Reyhanganteng@gmail.com'], // Cek apakah email sudah ada
+            [
+                'name' => 'Reyhan',
+                'password' => Hash::make('Reyhanmaulana123'),
+            ]
+        );
+        User::firstOrCreate(
+            ['email' => 'Rerey@gmail.com'], // Cek apakah email sudah ada
+            [
+                'name' => 'Rerey',
+                'password' => Hash::make('Reyhanmaulana123'),
+            ]
+        );
     }
 }
