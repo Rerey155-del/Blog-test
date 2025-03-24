76d8711bf4a40cc010c3f84ebd01b05f8de3dc6e
Reyhan Maulana <144551736+Rerey155-del@users.noreply.github.com>
2025-03-24 12:35:03 +0700

    oke

--- resources/views/admin/loginadmin.blade.php
+++ resources/views/admin/loginadmin.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login</title>
+    <title>Admin</title>
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
     @vite('resources/css/app.css')
@@ -24,8 +24,6 @@
             <input type="password" name="password" class="w-full h-14 bg-red-100 border-none rounded-lg px-8 text-sm font-medium text-red-500 mb-5 placeholder-gray-500" placeholder="Password" required>
     
             <button type="submit" class="w-full h-14 bg-red-500 text-white text-sm font-medium rounded-lg hover:bg-red-600 transition mb-5">Login</button>
-            <a href="#" class="pb-12">Forgot Password?</a>
-
             
             @if ($errors->any())
                 <div class="text-red-500 text-sm mt-2">
